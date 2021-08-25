268c01d0973f76ed5a44f34c5cea92a987d3d3d2
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-25 20:21:27 +0300

    Added different plot functions

--- scripts/plot.py
+++ scripts/plot.py
@@ -9,10 +9,68 @@ import logging.handlers
 from project_config import Config
 
 
-class plot():
+class Plot():
 
     def __init__(self):
         self._logger = logger
 
+    ################### PLOTTING FUNCTIONS #####################
 
-    
+    def plot_box(self, df:pd.DataFrame, x_col:str, title:str) -> None:
+        plt.figure(figsize=(12, 7))
+        sns.boxplot(data = df, x=x_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.show()
+
+    def plot_hist(self, df:pd.DataFrame, column:str, color:str)->None:
+        sns.displot(data=df, x=column, color=color, kde=True, height=4, aspect=2)
+        plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+        plt.show()
+
+
+    def plot_count(self, df:pd.DataFrame, column:str) -> None:
+        plt.figure(figsize=(12, 7))
+        sns.countplot(data=df, x=column)
+        plt.title(f'Distribution of {column}', size=20, fontweight='bold')
+        plt.show()
+        
+    def plot_bar(self, df:pd.DataFrame, x_col:str, y_col:str, title:str, xlabel:str, ylabel:str)->None:
+        plt.figure(figsize=(6, 4))
+        sns.barplot(data = df, x=x_col, y=y_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=45, fontsize=12)
+        plt.yticks( fontsize=14)
+        plt.xlabel(xlabel, fontsize=13)
+        plt.ylabel(ylabel, fontsize=13)
+        plt.show()
+
+    def plot_heatmap(self, df:pd.DataFrame, title:str, cbar=False)->None:
+        plt.figure(figsize=(12, 7))
+        sns.heatmap(df, annot=True, cmap='viridis', vmin=0, vmax=1, fmt='.2f', linewidths=.7, cbar=cbar )
+        plt.title(title, size=18, fontweight='bold')
+        plt.show()
+
+    def plot_box(self, df:pd.DataFrame, x_col:str, title:str) -> None:
+        plt.figure(figsize=(12, 7))
+        sns.boxplot(data = df, x=x_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.show()
+
+    def plot_box_multi(self, df:pd.DataFrame, x_col:str, y_col:str, title:str) -> None:
+        plt.figure(figsize=(12, 7))
+        sns.boxplot(data = df, x=x_col, y=y_col)
+        plt.title(title, size=20)
+        plt.xticks(rotation=75, fontsize=14)
+        plt.yticks( fontsize=14)
+        plt.show()
+
+    def plot_scatter(self, df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
+        plt.figure(figsize=(8, 5))
+        sns.scatterplot(data = df, x=x_col, y=y_col, hue=hue, style=style)
+        plt.title(title, size=20)
+        plt.xticks(fontsize=14)
+        plt.yticks( fontsize=14)
+        plt.show()
+

5785d2ec1df2a6a7a1dccf6b97d04cdceff6ab07
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-25 12:05:50 +0300

    fix: project directories and files paths

--- scripts/project_config.py
+++ scripts/project_config.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 class Config:
 
-  Root_Path = Path("./")
+  Root_Path = Path("../")
   Repository = "https://github.com/Caphace-Ethan/Breast-Cancer-Diagnosis"
-  Data_path = Root_Path / "data"
+  Data_path = Root_Path / "data/"
   Image_path = Data_path / "img"
 
   scripts_path = Root_Path / "scripts"
-
+  csv_data_path = Path("../data/data.csv")
 
 

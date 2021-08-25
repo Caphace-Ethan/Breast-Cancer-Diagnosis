ced984ccb20bf1cc4a340835f93aaf776d9837dd
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-25 11:18:41 +0300

    Added class plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,1 +1,18 @@
 # Graph and Diagram Ploting script.
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from log import logger
+import logging
+import logging.handlers
+
+from project_config import Config
+
+
+class plot():
+
+    def __init__(self):
+        self._logger = logger
+
+
+    

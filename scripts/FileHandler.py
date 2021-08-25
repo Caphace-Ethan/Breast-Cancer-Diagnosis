3ca362377735a9b81d17f53e461a6043510c4a95
Caphace-Ethan <ethanyoung135@gmail.com>
2021-08-25 10:07:31 +0300

    Updated FileHandler, log and config scripts in the project

---
+++ scripts/FileHandler.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from log import logger
+import logging
+import logging.handlers
+
+from project_config import Config
+
+class FileHandler():
+
+  def __init__(self):
+    self._logger = logging
+
+  def save_csv(self, df, name, index=False):
+    try:
+      path = Config.Data_path / str(name + '.csv')
+      df.to_csv(path, index=index)
+      self._logger.info(f"{name} file saved successfully in csv format")
+    except Exception:
+      self._logger.exception(f"{name} save failed")
+
+  def read_csv(self, name, missing_values=[]):
+    try:
+      path = Config.Data_path / str(name + '.csv')
+      df = pd.read_csv(path, na_values=missing_values)
+      self._logger.info(f"{name} file read successfully")
+      return df
+    except FileNotFoundError:
+      self._logger.exception(f"{name} not found")
+
--- scripts/project_config.py
+++ scripts/project_config.py
@@ -4,25 +4,11 @@ from pathlib import Path
 class Config:
 
   Root_Path = Path("./")
-  Repository = "https://github.com/Caphace-Ethan/Python_USGS_LIDAR"
+  Repository = "https://github.com/Caphace-Ethan/Breast-Cancer-Diagnosis"
   Data_path = Root_Path / "data"
-  Laz_path = Data_path / "laz"
-  Tif_path = Data_path / "tif"
-  Shp_path = Data_path / "shp"
   Image_path = Data_path / "img"
 
-  Test_data_path = Root_Path / "tests/test_data"
-  test_Laz_path = Test_data_path / "laz"
-  test_Tif_path = Test_data_path / "tif"
-  test_Shp_path = Test_data_path / "shp"
-  test_Image_path = Test_data_path / "img"
-
-  remote_data_path = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/IA_FullState/ept.json"
-  remote_data_directory = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
-  region_names = "usgs3dep_region_data"
-
-  pipeline_path = Root_Path / "data/pipeline.json"
-  test_pipeline_path = Root_Path / "tests/test_pipeline.json"
+  scripts_path = Root_Path / "scripts"
 
 
 
